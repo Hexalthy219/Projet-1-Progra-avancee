6826a285f65778ed6d63059f0ee626fdb7ae09a0
Cyril <cyril.russe@hotmail.com>
2020-10-22 03:50:54 +0200

    placesort + debut rapport

--- p1/PlaceSort.c
+++ p1/PlaceSort.c
@@ -6,6 +6,34 @@
 #include <stddef.h>
 #include "Sort.h"
 
-
-void sort(int *array, size_t length)
+static void swap(int *array, int a, int b)
 {
+    int temp = array[a];
+    array[a] = array[b];
+    array[b] = temp;
+}
+
+void sort(int *array, size_t length){
+    size_t i=0, m, x=0;
+    while(i<length){
+        x++;
+        m=0;
+        for(size_t j=0; j<length; j++){
+            if(array[i]>array[j])
+                m++;
+        }
+
+        if (m!=i){
+            if (array[i]==array[m]){
+                while (array[i]==array[m])
+                    m++;
+                swap(array, i, m);
+                i++;
+            }
+            else
+                swap(array, i, m);
+        }
+        else
+            i++;
+    }
+}
--- p1/moyenne.c
+++ p1/moyenne.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+static void affichage(int *array, size_t length)
+{
+    for (size_t i = 0; i < length; i++)
+        printf("%d\t", array[i]);
+    printf("\n\n");
+}
 
 int main(){
     double moyenne=0;
